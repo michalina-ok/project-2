59f0caf947cdf8d7a34670eabcddbbd7813a5d94
Michalina <michalina.oniszczuk@gmail.com>
2022-08-05 12:50:15 +0200

    finished project, not fully working

---
+++ script.js
@@ -0,0 +1,11 @@
+// pick the h1 tag
+const h1 = document.querySelector("h1")
+
+//and when I scroll, calculate how big the font size should be
+window.addEventListener("scroll", function() {
+    const pixels = window.pageYOffset;
+
+    const fontWeight = pixels * 0.4 + 100;
+    const fontWidth = pixels * 0.1 + 100;
+    h1.style.fontVariationSettings = `"wgth" ${fontWeight}, "wdth" ${fontWidth}`
+})
